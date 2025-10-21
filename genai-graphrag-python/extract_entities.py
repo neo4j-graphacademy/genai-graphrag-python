9240da244c23eeca97a8600dc044c2a561c39b44
Martin O'Hanlon <martin.ohanlon@neo4j.com>
2025-10-21 08:46:28 +0100

    extract schema

---
+++ genai-graphrag-python/extract_entities.py
@@ -0,0 +1,32 @@
+import asyncio
+from neo4j_graphrag.experimental.components.entity_relation_extractor import (
+    LLMEntityRelationExtractor,
+)
+from neo4j_graphrag.experimental.components.types import (
+    TextChunks,
+    TextChunk
+)
+from neo4j_graphrag.llm import OpenAILLM
+
+text = """
+Neo4j is a graph database that stores data in a graph structure.
+Data is stored as nodes and relationships instead of tables or documents.
+Graph databases are particularly useful when _the connections between data are as important as the data itself_.
+A graph shows how objects are related to each other.
+The objects are referred to as *nodes* (vertices) connected by *relationships* (edges).
+Neo4j uses the graph structure to store data and is known as a *labeled property graph*.
+"""
+
+extractor = LLMEntityRelationExtractor(
+    llm=OpenAILLM(
+        model_name="gpt-4"
+    )
+)
+
+entities = asyncio.run(
+    extractor.run(
+        chunks=TextChunks(chunks=[TextChunk(text=text, index=0)])
+    )
+)
+
+print(entities)
---
+++ genai-graphrag-python/extract_schema.py
@@ -0,0 +1,18 @@
+from neo4j_graphrag.experimental.components.schema import SchemaFromTextExtractor
+from neo4j_graphrag.llm import OpenAILLM
+import asyncio
+
+schema_extractor = SchemaFromTextExtractor(
+    llm=OpenAILLM(
+        model_name="gpt-4"
+    )
+)
+
+text = """
+Neo4j is a graph database management system (GDBMS) developed by Neo4j Inc.
+"""
+
+# Extract the schema from the text
+extracted_schema = asyncio.run(schema_extractor.run(text=text))
+
+print(extracted_schema)

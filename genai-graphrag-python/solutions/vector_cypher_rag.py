ccc6d60d880d670dfac60456e3109badcd6790e5
Martin O'Hanlon <martin.ohanlon@neo4j.com>
2026-03-26 11:18:01 +0000

    update example query

--- genai-graphrag-python/solutions/vector_cypher_rag.py
+++ genai-graphrag-python/solutions/vector_cypher_rag.py
@@ -80,7 +80,7 @@ llm = OpenAILLM(
 rag = GraphRAG(retriever=retriever, llm=llm)
 
 # Search
-query_text = "Where can I learn more about knowledge graphs?"
+query_text = "What is a knowledge graphs and where can I learn about them?"
 
 response = rag.search(
     query_text=query_text, 
@@ -99,6 +99,8 @@ driver.close()
 
 """
 # tag::example_queries[]
+query_text = "What is a knowledge graphs and where can I learn about them?"
 query_text = "What technologies and concepts support knowledge graphs?"
+query_text = "Describe Retrieval Augmented Generation."
 # end::example_queries[]
 """
